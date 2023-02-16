df55b6f18a918fe7ba3fd8c93f3b3831c0a6948f
DonovanZeanah <donovan.zeanah@outlook.com>
2023-02-16 03:08:13 -0600

    test cases passed for updating

--- IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins.InMemory/InventoryRepository.cs
@@ -16,10 +16,45 @@ namespace IMS.Plugins.InMemory
         new Inventory() { InventoryId = 4, InventoryName = "Lensclothe", Quantity = 100, Price = 1 },
       };
     }
+
+        public Task AddInventoryAsync(Inventory inventory)
+        {
+           if (_inventories.Any(x => x.InventoryName.Equals(inventory.InventoryName, StringComparison.OrdinalIgnoreCase)))
+            {
+                return Task.CompletedTask;
+            }
+
+            var maxId = _inventories.Max(x => x.InventoryId);
+            inventory.InventoryId = maxId + 1;
+            _inventories.Add(inventory);
+            return Task.CompletedTask;
+        }
+
+    public async Task<bool> ExistsAsync(Inventory inventory)
+    {
+      return await Task.FromResult(_inventories.Any(x => x.InventoryName.Equals(inventory.InventoryName, StringComparison.OrdinalIgnoreCase)));
+    }
+
     public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name = "")
     {
       if (string.IsNullOrWhiteSpace(name)) return await Task.FromResult(_inventories);
       return _inventories.Where(x => x.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public Task UpdateInventoryAsync(Inventory inventory)
+    {
+      //prevent duplicate inventory names
+      if (_inventories.Any(x => x.InventoryId != inventory.InventoryId && x.InventoryName.Equals(inventory.InventoryName, StringComparison.OrdinalIgnoreCase)))
+        return Task.CompletedTask;
+
+      var inv = _inventories.FirstOrDefault(x => x.InventoryId == inventory.InventoryId);
+      if (inv != null)
+      {
+        inv.InventoryName = inventory.InventoryName;
+        inv.Price = inventory.Price;
+        inv.Quantity = inventory.Quantity;
+      }
+      return Task.CompletedTask;
     }
   }
 }
